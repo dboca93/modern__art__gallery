19d9c667b055c48b683d5a114ad9ab2a0e2f0988
dboca93 <78838162+dboca93@users.noreply.github.com>
2024-01-30 11:31:00 +1100

    first__commit

---
+++ live.js
@@ -0,0 +1,11 @@
+
+
+function redirectToNextPage() {
+    // Use window.location.href to navigate to the next page
+    window.location.href = "location.html";
+}
+
+
+function redirect__Prev() {
+    window.location.href = "index.html";
+}
